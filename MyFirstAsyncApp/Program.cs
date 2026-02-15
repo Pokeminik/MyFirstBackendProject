6812ebd8b9245a1a640295a10696250e8394a39c
Pokeminik <pockemonchik3@gmail.com>
2026-02-15 19:14:23 +0200

    Clean commit without VS junk

---
+++ MyFirstAsyncApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        List<string> users = ["Ivan", "Oleg"];
+        users.Add("Dmitro");
+        Console.WriteLine("Починаю завантаження даних...");
+
+        string data = await FetchDataFromInternet();
+
+        Console.WriteLine($"Дані отримано: {data}");
+
+        foreach (var user in users)
+        {  
+            Console.WriteLine($"Користувач: {user.ToUpper()}"); 
+        }
+    }
+
+    static async Task<string> FetchDataFromInternet()
+    {
+        await Task.Delay(2000);
+        return "Успіх";
+    }
+}
