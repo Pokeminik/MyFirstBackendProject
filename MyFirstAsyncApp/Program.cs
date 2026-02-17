f219ad5e17a927a9736dc9dfaf6542b23bba6f81
Pokeminik <pockemonchik3@gmail.com>
2026-02-17 14:43:27 +0200

    Added LINQ filtering examples

--- MyFirstAsyncApp/Program.cs
+++ MyFirstAsyncApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyFirstAsyncApp;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -15,9 +16,27 @@ class Program
         Console.WriteLine($"Дані отримано: {data}");
 
         foreach (var user in users)
-        {  
-            Console.WriteLine($"Користувач: {user.ToUpper()}"); 
+        {
+            Console.WriteLine($"Користувач: {user.ToUpper()}");
         }
+
+        List<DriverLicense> licenses = new List<DriverLicense>
+        {
+        new DriverLicense { Id = 1, FirstName = "Ivan", LastName = "Sirko", Category = "B", IssueDate = new DateTime(2020, 05, 10) },
+        new DriverLicense { Id = 2, FirstName = "Oleg", LastName = "Honchar", Category = "C", IssueDate = new DateTime(2021, 08, 22) },
+        new DriverLicense { Id = 3, FirstName = "Dmitro", LastName = "Kozak", Category = "B", IssueDate = new DateTime(2019, 11, 01) }
+        };
+
+        var categoryB = licenses.Where(l => l.Category == "B").ToList();
+        var id2 = licenses.FirstOrDefault(l => l.Id == 2);
+
+        Console.WriteLine("Водії з категорією B:");
+
+        foreach (var lic in categoryB)
+        {
+            Console.WriteLine($"{lic.FirstName} {lic.LastName}");
+        }
+        Console.WriteLine($"Водій з ID2: {id2.FirstName} {id2.LastName}");
     }
 
     static async Task<string> FetchDataFromInternet()
